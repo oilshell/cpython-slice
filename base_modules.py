fb2a9a3a2d0024c02cceb52035804facedd02c13
Andy C <andy@chubot.org>
2017-04-29 21:24:24 -0700

    Module dependencies.  Zipfile brings in a ton more modules.

---
+++ base_modules.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python -S
+"""
+base_modules.py
+
+NOTE -S above
+"""
+
+import sys  # 15 modules
+import os  #  31 modules
+import runpy  # 34 modules
+# Hm this brings in the thread module, geez.
+import zipfile  # 62 modules
+
+
+def main(argv):
+  for k, v in sys.modules.iteritems():
+    print k, v
+
+
+if __name__ == '__main__':
+  try:
+    main(sys.argv)
+  except RuntimeError as e:
+    print >>sys.stderr, 'FATAL: %s' % e
+    sys.exit(1)
