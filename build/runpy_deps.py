61538383c7c66db5e83521514afa5e00b9a09514
Andy C <andy@chubot.org>
2017-05-08 22:01:39 -0700

    code cleanup

--- build/runpy_deps.py
+++ build/runpy_deps.py
@@ -27,27 +27,16 @@ def main(argv):
   stdlib_dir = runpy_path[: i + 1]  # include trailing slash
   stdlib_dir_len = len(stdlib_dir)
 
-  #print 'STDLIB', stdlib_dir
-  #return
-
   with open(py_out_path, 'w') as py_out, open(c_out_path, 'w') as c_out:
     for name in sorted(sys.modules):
       mod = sys.modules[name]
       if name in ('__builtin__', '__main__'):
         continue
 
-      #if '(built-in)' in str(mod):
-      #  continue
       try:
         full_path = mod.__file__
       except AttributeError:
         full_path = None
-      #if filename is None:
-      #  continue
-
-      # I think this is caught elsewhere
-      if full_path and full_path.endswith('.so'):
-        print '!!!'
 
       # If it's cached, it will be under .pyc; otherwise under .py.
       if full_path and full_path.endswith('.py'):
@@ -57,13 +46,12 @@ def main(argv):
         pyc_path = full_path
         py_path = full_path[:-1]
       else:
+        # Print a different format for C modules.
         print >>c_out, name, full_path
 
       if py_path:
-        #print('F', full_path, stdlib_dir)
         if py_path.startswith(stdlib_dir):
           rel_py_path = py_path[stdlib_dir_len:]
-          #print('REL', rel_path)
         else:
           rel_py_path = py_path
 
@@ -72,7 +60,6 @@ def main(argv):
         # .py file for tracebacks
         print >>py_out, py_path, rel_py_path
 
-      #filename = getattr(mod, '__file__', None)
   print >>sys.stderr, '-- Wrote %s and %s' % (py_out_path, c_out_path)
 
 
